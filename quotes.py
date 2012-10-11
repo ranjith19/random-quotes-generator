0ba7073a9098ff23955b9d97d912b33b7ca6f9ee
Ranjith Ramachandra <ranjith19@gmail.com>
2012-10-11 11:24:33 +0530

    Added files

---
+++ quotes.py
@@ -0,0 +1,134 @@
+from __future__ import print_function
+import random
+
+
+# System color name constants.
+(
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    LIGHT_GRAY,
+    DARK_GRAY,
+    BRIGHT_RED,
+    BRIGHT_GREEN,
+    BRIGHT_YELLOW,
+    BRIGHT_BLUE,
+    BRIGHT_MAGENTA,
+    BRIGHT_CYAN,
+    WHITE,
+) = range(16)
+
+def rgb(red, green, blue):
+    """
+    Calculate the palette index of a color in the 6x6x6 color cube.
+
+    The red, green and blue arguments may range from 0 to 5.
+    """
+    return 16 + (red * 36) + (green * 6) + blue
+
+def gray(value):
+    """
+    Calculate the palette index of a color in the grayscale ramp.
+
+    The value argument may range from 0 to 23.
+    """
+    return 232 + value
+
+def set_color(fg=None, bg=None):
+    """
+    Print escape codes to set the terminal color.
+
+    fg and bg are indices into the color palette for the foreground and
+    background colors.
+    """
+    print(_set_color(fg, bg), end='')
+
+def _set_color(fg=None, bg=None):
+    result = ''
+    if fg:
+        result += '\x1b[38;5;%dm' % fg
+    if bg:
+        result += '\x1b[48;5;%dm' % bg
+    return result
+
+def reset_color():
+    """
+    Reset terminal color to default.
+    """
+    print(_reset_color(), end='')
+
+def _reset_color():
+    return '\x1b[0m'
+
+def print_color(*args, **kwargs):
+    """
+    Print function, with extra arguments fg and bg to set colors.
+    """
+    fg = kwargs.pop('fg', None)
+    bg = kwargs.pop('bg', None)
+    set_color(fg, bg)
+    print(*args, **kwargs)
+    reset_color()
+
+def format_color(string, fg=None, bg=None):
+    return _set_color(fg, bg) + string + _reset_color()
+
+def get_random_color():
+    def get_random():
+        return random.choice(range(6))
+    c1 = rgb(get_random(), get_random(), get_random())
+    c2 = random.choice(range(1,15))
+    return random.choice([c1,c2])
+    
+
+f = open('.quotes.txt', 'r')
+txt = f.read()
+lines = txt.split('\n.\n')
+
+print_color(random.choice(lines), fg=get_random_color())
+#print("\033[{0}m{1}{2}".format(random.choice(range(31,36)), random.choice(lines),  default))
+
+"""
+if __name__ == '__main__':
+    # Print a test graphic showing all colors.
+    print('System colors:')
+    for c in range(8):
+        print_color('  ', bg=c, end='')
+    print()
+    for c in range(8, 16):
+        print_color('  ', bg=c, end='')
+    print()
+    print()
+
+    print('RGB color cube, 6x6x6:')
+    for green in range(6):
+        for red in range(6):
+            for blue in range(6):
+                print_color('  ', bg=rgb(red, green, blue), end='')
+            print(' ', end='')
+        print()
+    print()
+
+    print('Grayscale ramp, with RGB grays:')
+    for value in range(24):
+        print_color('  ', bg=gray(value), end='')
+    print()
+
+    print_color('  ', bg=rgb(0, 0, 0), end='')
+    print('  '*7, end='')
+    print_color('    ', bg=rgb(1, 1, 1), end='')
+    print('  '*2, end='')
+    print_color('    ', bg=rgb(2, 2, 2), end='')
+    print('  '*2, end='')
+    print_color('    ', bg=rgb(3, 3, 3), end='')
+    print('  '*2, end='')
+    print_color('    ', bg=rgb(4, 4, 4), end='')
+    print('  ', end='')
+    print_color('  ', bg=rgb(5, 5, 5), end='')
+    print()
+    """
+
